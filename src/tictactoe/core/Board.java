3b8da4f8c8f272e00df937b3a2dd5f24c6e0f3cb
João vitor <contato.jhonnybjj@gmail.com>
2025-05-25 15:17:25 -0300

    Ajustes para identificar o vencedor

--- src/tictactoe/core/Board.java
+++ src/tictactoe/core/Board.java
@@ -69,11 +69,6 @@ public class Board {
 
     }
 
-    private Symbol findSequence() {
-        //TODO
-        return null;
-    }
-
     public boolean isFull() {
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
@@ -83,5 +78,73 @@ public class Board {
             }
         }
         return true;
+    }
+
+    private Symbol findSequence() {
+        Symbol symbol = findSequenceInRows();
+        if (symbol != null){
+            return symbol;
+        }
+        symbol = findSequenceInColumns();
+        if (symbol != null){
+            return symbol;
+        }
+
+        return findSequenceInDiagonals();
+
+    }
+
+    private Symbol findSequenceInRows() {
+        for (int i = 0; i < SIZE; i ++) {
+           Symbol symbol = findSequenceInRow(i);
+           if (symbol != null) {
+               return symbol;
+           }
+        }
+        return null;
+    }
+
+    private Symbol findSequenceInRow(int i) {
+        return matrix[i][0] == matrix[i][1]
+                && matrix[i][1] == matrix[i][2]
+                && matrix[i][0] != Symbol.NONE
+                ? matrix[i][0]
+                : null;
+    }
+
+    private Symbol findSequenceInColumns() {
+        for (int j = 0; j < SIZE; j ++) {
+            Symbol symbol = findSequenceInColumn(j);
+            if (symbol != null) {
+                return symbol;
+            }
+        }
+        return null;
+    }
+
+    private Symbol findSequenceInColumn(int j) {
+        return matrix[0][j] == matrix[1][j]
+                && matrix[1][j] == matrix[2][j]
+                && matrix[0][j] != Symbol.NONE
+                ? matrix[0][j]
+                : null;
+    }
+
+    private Symbol findSequenceInDiagonals() {
+        if (matrix[0][0] == matrix[1][1]
+                && matrix[1][1] == matrix[2][2]
+                && matrix[0][0] != Symbol.NONE
+        ){
+            return matrix[0][0];
+        }
+
+        if (matrix[0][2] == matrix[1][1]
+                && matrix[1][1] == matrix[2][0]
+                && matrix[0][2] != Symbol.NONE
+        ){
+            return matrix[0][2];
+        }
+
+        return null;
     }
 }
