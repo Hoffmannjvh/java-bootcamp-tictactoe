54b480a5ec5cf2c9d00e2fca0e7b17e5d54e76f1
João vitor <contato.jhonnybjj@gmail.com>
2025-05-25 14:40:49 -0300

    Ajustes no esqueleto do jogo

--- src/tictactoe/core/Game.java
+++ src/tictactoe/core/Game.java
@@ -1,15 +1,34 @@
 package tictactoe.core;
 
+import tictactoe.io.Input;
+import tictactoe.io.Output;
+
 public class Game {
 
     private final Board board = new Board();
     private final Players players = new Players();
 
     public void start() {
-        System.out.println("Jogo iniciado");
 
-        System.out.println(board);
+        while (true){
+            Output.writeNewLine();
+            Output.write(board);
+            play(players.next());
+        }
 
+    }
+
+    private void play (Symbol symbol) {
+        while (true) {
+            try {
+                String play =  Input.read(String.format("'%s' Play =>", symbol));
+                Coord coord = Coord.parse(play);
+                board.update(symbol, coord);
+                break;
+            } catch (RuntimeException e) {
+                Output.write("ERROR: " + e.getMessage());
+            }
+        }
     }
 
 }
